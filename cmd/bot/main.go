3f55b885336499316efaf984c2f0324fce966ab3
snikolchikov <snikolchikov@utrace.ru>
2021-05-25 00:12:10 +0300

    refactor

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
+	"weatherbot/pkg/telegram"
 )
 
 func main() {
@@ -13,23 +14,8 @@ func main() {
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+	newBot := telegram.NewBot(bot)
+	if err := newBot.Start(); err != nil {
+		log.Fatal(err)
 	}
 }
